88f77fd813fba6c06587de1bf6c23108fcae512c
Joshua Coales <joshua@coales.co.uk>
2020-06-18 23:38:54 +0100

    Adding template database class

---
+++ database.py
@@ -0,0 +1,15 @@
+class Database:
+    def __init__(self, db_config):
+        self.conn = ""
+
+    def get_message_history(self, message_id):
+        pass
+
+    def get_message_family(self, message_id):
+        pass
+
+    def save_message(self, message):
+        pass
+
+    def get_messages_matching_hashes(self, hashes):
+        pass
