2fa07cc0b9cfcb0357e8c98e515377639af14f75
Joshua Coales <joshua@coales.co.uk>
2020-06-30 15:23:10 +0100

    Reorganising Group.from_config()

--- group.py
+++ group.py
@@ -3,14 +3,17 @@ from __future__ import annotations
 import logging
 import os
 from abc import ABC, abstractmethod
-from typing import Dict, Union, Any, TypeVar, List, Optional
+from typing import Dict, Union, Any, TypeVar, List, Optional, Coroutine, Callable
 from typing import TYPE_CHECKING
+
+from message import Message
 
 if TYPE_CHECKING:
     from telegram_client import TelegramClient
     from database import Database
-    from message import MessageData, Message
+    from message import MessageData
 T = TypeVar('T', bound='Group')
+C = TypeVar('C', bound='ChatData')
 
 
 class ChatConfig(ABC):
@@ -22,6 +25,9 @@ class ChatConfig(ABC):
     @abstractmethod
     def from_json(json_dict: Dict[str, Any]) -> 'ChatConfig':
         pass
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(handle={self.handle})"
 
 
 class ChannelConfig(ChatConfig):
@@ -49,9 +55,9 @@ class ChatData(ABC):
     def directory(self) -> str:
         pass
 
+    @abstractmethod
     def telegram_link_for_message(self, message_data: 'MessageData') -> str:
-        handle = abs(self.chat_id)
-        return f"https://t.me/c/{handle}/{message_data.message_id}"
+        pass
 
 
 class ChannelData(ChatData):
@@ -70,6 +76,10 @@ class WorkshopData(ChatData):
     def directory(self) -> str:
         return f"store/workshop/{self.chat_id}/"
 
+    def telegram_link_for_message(self, message_data: 'MessageData') -> str:
+        handle = abs(self.chat_id)
+        return f"https://t.me/c/{handle}/{message_data.message_id}"
+
 
 class Group(ABC):
     def __init__(
@@ -84,13 +94,26 @@ class Group(ABC):
         self.messages = messages
         self.client = client
 
-    @classmethod
-    async def from_config(cls, config: 'ChatConfig', client: TelegramClient, database: 'Database') -> Group:
-        logging.info(f"Initialising channel: {config}")
+    @staticmethod
+    async def load_chat_data(
+            getter: Callable[[str], Coroutine[None, None, C]],
+            config: 'ChatConfig',
+            client: TelegramClient,
+            database: 'Database'
+    ) -> C:
         # Get chat id, name, etc
-        chat_data = await client.get_chat_data(config.handle)
+        chat_data = await getter(config.handle)
         # Ensure chat is in database
         database.save_chat(chat_data)
+        # Create directory
+        os.makedirs(chat_data.directory, exist_ok=True)
+        return chat_data
+
+    @staticmethod
+    async def load_messages(
+            chat_data: 'ChatData', config: 'ChatConfig', client: TelegramClient, database: 'Database'
+    ) -> List[Message]:
+        logging.info(f"Initialising channel: {config}")
         # Get messages from database and channel, ensure they match
         database_messages = database.list_messages_for_chat(chat_data)
         channel_messages = [m async for m in client.iter_channel_messages(chat_data)]
@@ -100,12 +123,12 @@ class Group(ABC):
             database.save_message(message_data)
         for message_data in removed_messages:
             database.remove_message(message_data)
-        # Create directors
-        os.makedirs(chat_data.directory, exist_ok=True)
         # Check files, turn message data into messages
         messages = []
         for message in channel_messages:
-            messages.append(await Message.from_message_data(message, chat_data, client))
+            new_message = await Message.from_message_data(message, chat_data, client)
+            messages.append(new_message)
+            database.save_message(new_message.message_data)
         # Check for extra files which need removing
         dir_files = os.listdir(chat_data.directory)
         msg_files = [msg.message_data.file_path for msg in messages]
@@ -116,7 +139,7 @@ class Group(ABC):
             except OSError:
                 pass
         # Return group
-        return cls(chat_data, config.queue, messages, client)
+        return messages
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.chat_data.title})"
@@ -124,16 +147,25 @@ class Group(ABC):
     def remove_message(self, message_data: MessageData) -> None:
         self.messages = [msg for msg in self.messages if msg.message_data == message_data]
 
+    @classmethod
+    @abstractmethod
+    async def from_config(cls, config: 'ChatConfig', client: TelegramClient, database: 'Database'):
+        pass
+
 
 class Channel(Group):
 
     @classmethod
     async def from_config(cls, config: 'ChatConfig', client: TelegramClient, database: 'Database') -> Channel:
-        ...
+        chat_data = await Group.load_chat_data(client.get_channel_data, config, client, database)
+        messages = await Group.load_messages(chat_data, config, client, database)
+        return Channel(chat_data, config.queue, messages, client)
 
 
 class WorkshopGroup(Group):
 
     @classmethod
     async def from_config(cls, config: 'ChatConfig', client: TelegramClient, database: 'Database') -> WorkshopGroup:
-        ...
+        chat_data = await Group.load_chat_data(client.get_workshop_data, config, client, database)
+        messages = await Group.load_messages(chat_data, config, client, database)
+        return WorkshopGroup(chat_data, config.queue, messages, client)
