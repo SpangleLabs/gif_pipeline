1b6b1fdaf38ed57c7e4c859f6dddd65f6c6aedd4
Joshua Coales <joshua@coales.co.uk>
2020-07-10 23:05:25 +0100

    Making the merge helper resize videos and pad them before merging

--- helpers/merge_helper.py
+++ helpers/merge_helper.py
@@ -1,9 +1,12 @@
-from typing import Optional, List
+import asyncio
+import shutil
+from typing import Optional, List, Tuple
 
 from group import Group
 from helpers.helpers import Helper, random_sandbox_video_path
 from message import Message
 from tasks.ffmpeg_task import FfmpegTask
+from tasks.ffmprobe_task import FFprobeTask
 
 
 class MergeHelper(Helper):
@@ -95,15 +98,53 @@ class MergeHelper(Helper):
                 "Merge commands require at least 2 videos to merge. " \
                 "Please reply to a message, and provide telegram links to the other messages"
             return [await self.send_text_reply(chat, cmd_message, error_text)]
-        file_paths = [m.message_data.file_path for m in messages_to_merge]
-        num_files = len(file_paths)
+        num_files = len(messages_to_merge)
         filer_args = " ".join([f"[{x}:v] [{x}:a]" for x in range(num_files)])
-        output_args = f"-filter_complex \"{filer_args} concat=n={num_files}:v=1:a=1 [v] [a] -map \"[v]\" -map \"[a]\""
-        output_path = random_sandbox_video_path()
-        task = FfmpegTask(
-            inputs={file_path: None for file_path in file_paths},
-            outputs={output_path: output_args}
-        )
+        output_args = f"-filter_complex \"{filer_args} concat=n={num_files}:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\""
         async with self.progress_message(chat, cmd_message, "Merging videos"):
+            file_paths = await self.align_video_dimensions([m.message_data.file_path for m in messages_to_merge])
+            output_path = random_sandbox_video_path()
+            task = FfmpegTask(
+                inputs={file_path: None for file_path in file_paths},
+                outputs={output_path: output_args}
+            )
             await self.worker.await_task(task)
             return [await self.send_video_reply(chat, cmd_message, output_path)]
+
+    async def align_video_dimensions(self, file_paths: List[str]) -> List[str]:
+        first = file_paths[0]
+        the_rest = file_paths[1:]
+        # Get dimensions of first video, scale the rest to match
+        dimensions = await self.get_video_dimensions(first)
+        rescaled = await asyncio.gather(*[self.scale_and_pad_to_dimensions(path, dimensions) for path in the_rest])
+        same_dimension_paths = [first] + rescaled
+        # Handle duplicate file paths. Copy them to sandbox files
+        output_paths = []
+        for path in same_dimension_paths:
+            if path in output_paths:
+                new_path = random_sandbox_video_path(path.split(".")[-1])
+                shutil.copyfile(path, new_path)
+                output_paths.append(new_path)
+            else:
+                output_paths.append(path)
+        return output_paths
+
+    async def get_video_dimensions(self, file_path: str) -> Tuple[int, int]:
+        task = FFprobeTask(
+            inputs={file_path: "-v error -show_entries stream=width,height -of csv=p=0:s=x"}
+        )
+        return tuple((await self.worker.await_task(task)).split("x"))
+
+    async def scale_and_pad_to_dimensions(self, file_path: str, dimensions: Tuple[int, int]) -> str:
+        orig_dimensions = await self.get_video_dimensions(file_path)
+        if orig_dimensions == dimensions:
+            return file_path
+        output_path = random_sandbox_video_path()
+        x, y = dimensions
+        args = f"-vf \"scale={x}:{y}:force_original_aspect_ratio=decrease,pad={x}:{y}:(ow-iw)/2:(oh-ih)/2,setsar=1\""
+        task = FfmpegTask(
+            inputs={file_path: None},
+            outputs={output_path: args}
+        )
+        await self.worker.await_task(task)
+        return output_path
