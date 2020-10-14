8c04161bd683ea7df0a735afd56a57ef279d7ba4
Joshua Coales <joshua@coales.co.uk>
2020-10-15 00:05:41 +0100

    ffmpeg_task to return output and error

--- tasks/ffmpeg_task.py
+++ tasks/ffmpeg_task.py
@@ -1,11 +1,12 @@
 import subprocess
+from typing import Tuple
 
 import ffmpy3
 
 from tasks.task import Task
 
 
-class FfmpegTask(Task[str]):
+class FfmpegTask(Task[Tuple[str, str]]):
 
     def __init__(self, *, global_options=None, inputs=None, outputs=None):
         self.global_options = global_options
@@ -18,8 +19,9 @@ class FfmpegTask(Task[str]):
             inputs=self.inputs,
             outputs=self.outputs
         )
-        ff_process = await ff.run_async(stdout=subprocess.PIPE)
+        ff_process = await ff.run_async(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         ff_out = await ff_process.communicate()
         await ff.wait()
         output = ff_out[0].decode('utf-8').strip()
-        return output
+        error = ff_out[1].decode('utf-8').strip()
+        return output, error
