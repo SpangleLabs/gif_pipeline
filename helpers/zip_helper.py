dad592618f853f645d12d8fb2f86a58fc2fc7e6a
Joshua Coales <joshua@coales.co.uk>
2020-10-16 21:32:54 +0100

    Automatically convert files as they come out of the zip

--- helpers/zip_helper.py
+++ helpers/zip_helper.py
@@ -6,10 +6,12 @@ from typing import Optional, List
 
 from group import Group
 from helpers.helpers import Helper, random_sandbox_video_path
+from helpers.telegram_gif_helper import TelegramGifHelper
 from message import Message, mime_type_is_video
+from tasks.ffmpeg_task import FfmpegTask
 
 
-class ZipHelper(Helper):
+class ZipHelper(TelegramGifHelper):
     async def on_new_message(self, chat: Group, message: Message) -> Optional[List[Message]]:
         if message.message_data.has_file and message.message_data.file_path.endswith(".zip"):
             results = await self.unzip(chat, message)
@@ -28,6 +30,21 @@ class ZipHelper(Helper):
                     with zip_ref.open(filename) as zf, open(video_path, "wb") as f:
                         shutil.copyfileobj(zf, f)
                     video_paths.append(video_path)
-        if video_paths:
-            return await asyncio.gather(*[self.send_video_reply(chat, message, path) for path in video_paths])
+        # Convert to mp4s
+        processed_paths = asyncio.gather(*(self.convert_file(path) for path in video_paths))
+        # Send them
+        if processed_paths:
+            return await asyncio.gather(*(self.send_video_reply(chat, message, path) for path in processed_paths))
         return None
+
+    async def convert_file(self, video_path: str) -> str:
+        if video_path.endswith(".gif"):
+            return await self.convert_video_to_telegram_gif(video_path)
+        else:
+            processed_path = random_sandbox_video_path()
+            task = FfmpegTask(
+                inputs={video_path: None},
+                outputs={processed_path: "-qscale 0"}
+            )
+            await self.worker.await_task(task)
+            return processed_path
