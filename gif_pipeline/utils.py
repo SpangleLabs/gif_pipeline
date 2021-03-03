3bb8c35b144519ecffa254666f2814e2623254f8
Joshua Coales <joshua@coales.co.uk>
2021-03-03 01:12:08 +0000

    Passing kwargs to tqdm_gather

--- gif_pipeline/utils.py
+++ gif_pipeline/utils.py
@@ -6,13 +6,15 @@ from tqdm import tqdm
 T = TypeVar("T")
 
 
-async def tqdm_gather(awaitables: List[Awaitable[T]]) -> List[T]:
+async def tqdm_gather(awaitables: List[Awaitable[T]], **kwargs) -> List[T]:
     async def wrap_awaitable(number: int, awaitable: Awaitable[T]):
         return number, await awaitable
 
     numbered_awaitables = [wrap_awaitable(idx, awaitables[idx]) for idx in range(len(awaitables))]
 
-    numbered_results = [await f for f in tqdm(asyncio.as_completed(numbered_awaitables), total=len(awaitables))]
+    numbered_results = [
+        await f for f in tqdm(asyncio.as_completed(numbered_awaitables), total=len(awaitables), **kwargs)
+    ]
 
     results = [result_tuple[1] for result_tuple in sorted(numbered_results)]
 
