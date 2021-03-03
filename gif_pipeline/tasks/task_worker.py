9cbd4a69b7a163ae6d971e5591b08d7559574bf1
Joshua Coales <joshua@coales.co.uk>
2021-03-03 01:11:53 +0000

    Adding Bottleneck class, an abstract TaskWorker

--- gif_pipeline/tasks/task_worker.py
+++ gif_pipeline/tasks/task_worker.py
@@ -1,18 +1,24 @@
 import asyncio
-from typing import List
+from typing import List, Awaitable
 
 from gif_pipeline.tasks.task import Task, T
 
 
-class TaskWorker:
+class Bottleneck:
 
-    def __init__(self, num_concurrent):
+    def __init__(self, num_concurrent: int):
         self.num_concurrent = num_concurrent
         self.semaphore = asyncio.Semaphore(num_concurrent)
 
-    async def await_task(self, task: Task[T]) -> T:
+    async def await_run(self, awaitable: Awaitable[T]) -> T:
         async with self.semaphore:
-            return await task.run()
+            return await awaitable
+
+
+class TaskWorker(Bottleneck):
+
+    async def await_task(self, task: Task[T]) -> T:
+        return await self.await_run(task.run())
 
     async def await_tasks(self, tasks: List[Task]):
         return await asyncio.gather(*[self.await_task(task) for task in tasks])
