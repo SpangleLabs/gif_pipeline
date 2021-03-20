68e415e260cd32bffd9aee7fc9711761db93f143
Joshua Coales <joshua@coales.co.uk>
2021-03-20 13:53:24 +0000

    Adding tag value menu class lookup based on tag type

--- gif_pipeline/helpers/menu_helper.py
+++ gif_pipeline/helpers/menu_helper.py
@@ -5,6 +5,7 @@ from typing import Optional, List, Tuple, Set
 from scenedetect import FrameTimecode
 from telethon import Button
 
+from gif_pipeline.chat_config import TagType
 from gif_pipeline.database import Database
 from gif_pipeline.chat import Chat, Channel
 from gif_pipeline.helpers.helpers import Helper
@@ -141,7 +142,13 @@ class MenuHelper(Helper):
             destination: Channel,
             tag_name: str
     ):
-        menu = EditTagValuesMenu(self, chat, cmd_msg, video, send_helper, destination, self.tag_manager, tag_name)
+        menu_class = {
+            TagType.NORMAL: EditTagValuesMenu,
+            TagType.SINGLE: EditTagValuesMenu,
+            TagType.TEXT: EditTagValuesMenu,
+            TagType.GNOSTIC: EditTagValuesMenu
+        }[destination.config.tags[tag_name].type]
+        menu = menu_class(self, chat, cmd_msg, video, send_helper, destination, self.tag_manager, tag_name)
         menu_msg = await menu.send()
         return [menu_msg]
 
