d333d2dbdc4d92028e890282ce8a7f7bf65f8dcc
Joshua Coales <joshua@coales.co.uk>
2021-04-13 22:37:57 +0100

    Adding schedule configuration for queues

--- gif_pipeline/chat_config.py
+++ gif_pipeline/chat_config.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
+from datetime import timedelta
 from enum import Enum
-from typing import Union, Dict, Any, Optional, List
+from typing import Union, Dict, Any, Optional
+
+import isodate
 
 
 class TagType(Enum):
@@ -15,7 +18,6 @@ class TagConfig:
         self.type = tag_type
 
     @staticmethod
-    @abstractmethod
     def from_json(json_dict: Dict[str, Any]) -> 'TagConfig':
         tag_type_str = json_dict.get("type", "normal")
         try:
@@ -26,6 +28,46 @@ class TagConfig:
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(type={self.type})"
+
+
+class ScheduleOrder(Enum):
+    RANDOM = "random"
+    OLDEST_FIRST = "oldest_first"
+    NEWEST_FIRST = "newest_first"
+
+
+class ScheduleConfig:
+    def __init__(
+            self,
+            min_time: timedelta,
+            *,
+            max_time: timedelta = None,
+            order: ScheduleOrder = None,
+    ):
+        self.min_time = min_time
+        self.max_time = max_time
+        self.order = order or ScheduleOrder.RANDOM
+
+    @staticmethod
+    def from_json(json_dict: Dict[str, Any]) -> 'ScheduleConfig':
+        min_time_str = json_dict["min_time"]
+        max_time_str = json_dict.get("max_time")
+        order_str = json_dict.get("order")
+        min_time = isodate.parse_duration(min_time_str)
+        max_time = None
+        if max_time_str:
+            max_time = isodate.parse_duration(max_time_str)
+        order = None
+        if order_str:
+            try:
+                order = ScheduleOrder[order_str.upper()]
+            except KeyError as e:
+                raise KeyError(f"Invalid schedule order, \"{order_str}\": {e}")
+        return ScheduleConfig(
+            min_time=min_time,
+            max_time=max_time,
+            order=order
+        )
 
 
 class ChatConfig(ABC):
@@ -105,14 +147,21 @@ class QueueConfig(WorkshopConfig):
             channel_handle: Union[str, int],
             *,
             duplicate_detection: bool = True,
+            schedule: ScheduleConfig = None
     ):
         super().__init__(handle, duplicate_detection=duplicate_detection)
         self.channel_handle = channel_handle
+        self.schedule = schedule
 
     @staticmethod
     def from_json(json_dict: Dict[str, Any], channel_handle: Union[str, int]) -> 'QueueConfig':
+        schedule = None
+        schedule_val = json_dict.get("schedule")
+        if schedule_val:
+            schedule = ScheduleConfig.from_json(schedule_val)
         return QueueConfig(
             json_dict["handle"],
             channel_handle,
             duplicate_detection=json_dict.get("duplicate_detection", True),
+            schedule=schedule
         )
