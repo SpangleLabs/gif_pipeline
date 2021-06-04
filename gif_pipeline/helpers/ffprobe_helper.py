bd48b6b1ced7b9c8921ac118e4c6663c519da243
Joshua Coales <joshua@coales.co.uk>
2021-06-04 12:02:20 +0100

    Adding resolution command to ffprobe helper

--- gif_pipeline/helpers/ffprobe_helper.py
+++ gif_pipeline/helpers/ffprobe_helper.py
@@ -10,34 +10,40 @@ class FFProbeHelper(Helper):
     async def on_new_message(self, chat: Chat, message: Message) -> Optional[List[Message]]:
         # If a message has text saying ffprobe or stats, and is a reply to a video, get stats for that video
         clean_text = message.text.strip().lower()
+        video = find_video_for_message(chat, message)
         if clean_text.startswith("ffprobe") or clean_text.startswith("stats"):
-            video = find_video_for_message(chat, message)
             if video is not None:
                 async with self.progress_message(chat, message, "Getting video stats"):
                     stats = await self.stats_for_video(video.message_data.file_path)
-                    stats_reply = await self.send_text_reply(chat, message, stats)
-                return [stats_reply]
-            reply = await self.send_text_reply(
+                    return [await self.send_text_reply(chat, message, stats)]
+            return [await self.send_text_reply(
                 chat,
                 message,
                 "Cannot work out which video you want stats about. "
                 "Please reply to the video you want to get stats for with the message \"ffprobe\"."
-            )
-            return [reply]
+            )]
         if clean_text.startswith("duration"):
-            video = find_video_for_message(chat, message)
             if video is not None:
                 async with self.progress_message(chat, message, "Getting video duration"):
                     duration = await self.duration_video(video.message_data.file_path)
-                    stats_reply = await self.send_text_reply(chat, message, f"{duration} seconds")
-                return [stats_reply]
-            reply = await self.send_text_reply(
+                    return [await self.send_text_reply(chat, message, f"{duration} seconds")]
+            return [await self.send_text_reply(
                 chat,
                 message,
                 "Cannot work out which video you want the duration of. "
                 "Please reply to the video you want to know the duration of with the message \"duration\"."
-            )
-            return [reply]
+            )]
+        if clean_test.startswith("resolution") or clean_text.startswith("size"):
+            if video is not None:
+                async with self.progress_message(chat, message, "Getting video resolution"):
+                    resolution = await self.video_resolution(video.message_data.file_path)
+                    return [await self.send_text_reply(chat, message, f"{resolution[0]} x {resolution[1]}")]
+               return [await self.send_text_reply(
+                   chat,
+                   message,
+                   "Cannot work out which video you want the resolution of."
+                   "Please reply to the video you want to know the resolution of with the message \"resolution\"."
+               )]
         # Otherwise, ignore
         return
 
@@ -54,3 +60,12 @@ class FFProbeHelper(Helper):
             inputs={video_path: "-show_entries format=duration -of default=noprint_wrappers=1:nokey=1"}
         )
         return float(await self.worker.await_task(probe_task))
+    
+    async def video_resolution(self, video_path: str) -> Tuple[int, int]:
+        probe_task = FFprobeTask(
+            global_options=["-v error"],
+            inputs={video_path: "-show_entries stream=width,height -of csv=p=0:s=x"}
+        )
+        resolution_str = await self.worker.await_task(probe_task)
+        resolution_split = resolution_str.split("x")
+        return int(resolution_split[0]), int(resolution_split[1]))
