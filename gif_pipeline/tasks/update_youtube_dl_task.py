77607eb03fc984f9fcd0f85fda62bacf1a4d4ad3
Joshua Coales <joshua@coales.co.uk>
2021-09-28 19:33:14 +0100

    Tell user the current youtube downloader version after checking for update

--- gif_pipeline/tasks/update_youtube_dl_task.py
+++ gif_pipeline/tasks/update_youtube_dl_task.py
@@ -1,3 +1,5 @@
+import re
+
 from gif_pipeline.tasks.task import Task, run_subprocess, TaskException
 from gif_pipeline.tasks.youtube_dl_task import yt_dl_pkg
 
@@ -15,8 +17,11 @@ class UpdateYoutubeDLTask(Task[str]):
             if yt_update:
                 return f"Updated {yt_dl_pkg} to {yt_update}"
             return "Updated other packages"
-        up_to_date_msg = f"Requirement already up-to-date: {yt_dl_pkg}"
-        yt_up_to_date = next(filter(lambda l: l.startswith(up_to_date_msg), lines), None)
+        up_to_date_regex = re.compile(
+            f"^Requirement already up-to-date: {re.escape(yt_dl_pkg)} .* \\(([^)]+)\\)$",
+            re.MULTILINE
+        )
+        yt_up_to_date = up_to_date_regex.search(stdout)
         if yt_up_to_date is not None:
-            return "Already up to date"
+            return f"Already up to date. Version: {yt_up_to_date.group(1)}"
         raise TaskException("Unknown response from pip")
