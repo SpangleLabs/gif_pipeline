e74d45917d0558464382502ae7b92821275e8e1b
Joshua Coales <joshua@coales.co.uk>
2022-05-12 23:45:01 +0100

    Fixing imports in text formatter to avoid cyclic imports

--- gif_pipeline/text_formatter.py
+++ gif_pipeline/text_formatter.py
@@ -1,15 +1,17 @@
-import re
 import string
 from collections import defaultdict
 
-from gif_pipeline.video_tags import VideoTags
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from gif_pipeline.video_tags import VideoTags
 
 
 class TextFormatter:
     def __init__(self, text: str):
         self.text = text
 
-    def format(self, tags: VideoTags) -> str:
+    def format(self, tags: "VideoTags") -> str:
         tag_dict = defaultdict(str)
         for tag_name in tags.list_tag_names():
             tag_dict[tag_name] = ", ".join(tags.list_values_for_tag(tag_name))
