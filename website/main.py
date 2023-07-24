6b81b4f30798d37ee1dacfc0860b7227b014b436
Spangle <dr-spangle@dr-spangle.com>
2023-07-25 00:09:25 +0100

    Throwing a fistful more garbage code at the wall

--- website/main.py
+++ website/main.py
@@ -1,23 +1,37 @@
 import json
 
+import flask
 from flask import Flask, Response
 
+from gif_pipeline.database import Database
 from gif_pipeline.pipeline import PipelineConfig
 
 app = Flask(__name__)
-with open("config.json", "r") as c:
+with open("../config.json", "r") as c:
     CONF = json.load(c)
 pipeline_conf = PipelineConfig(CONF)
+database = Database()
 
 
 @app.route("/tags/<chat_id>.json")
-def api_channel_tags(chat_id: str) -> flask.Response:
+def api_channel_tags(chat_id: str) -> Response:
+    chat_data = database.list_channels()
+    found_chat = None
+    for chat_datum in chat_data:
+        if chat_datum.matches_handle(chat_id):
+            found_chat = chat_datum
+            break
+    if found_chat is None:
+        return flask.jsonify({"error": "Chat not found"}), 404
+        return
+
+    messages = []
     # TODO: database.get_tags_for_message(self.message_data)
     pass  # TODO
 
 
 @app.route("/chats.json")
-def api_chat_list() -> flask.Response:
+def api_chat_list() -> Response:
     channels = [channel for channel in pipeline_conf.channels if channel.website_config.publicly_listed]
     channel_data = []
     for channel in channels:
